aecc08430aa728e29b5f03f18febabdedc132c1e
hosein rahnama <hosein.rahnama@outlook.com>
2025-11-26 17:03:15 +0330

    Improve runner output formatting

--- problem-set/1768/main.py
+++ problem-set/1768/main.py
@@ -1,17 +1,17 @@
 # 1768. Merge Strings Alternately
 
 class Solution:    
-    def mergeAlternately(self, word1: str, word2: str) -> str:
-        merged_word = ""
-        n1 = len(word1)
-        n2 = len(word2)
+    def mergeAlternately(self, string1: str, string2: str) -> str:
+        merged_string = ""
+        n1 = len(string1)
+        n2 = len(string2)
         l = min(n1, n2)
 
         for i in range(l):
-            merged_word = merged_word + word1[i] + word2[i]
+            merged_string += string1[i] + string2[i]
         for i in range(l, n1):
-            merged_word += word1[i]
+            merged_string += string1[i]
         for i in range(l, n2):
-            merged_word += word2[i]
+            merged_string += string2[i]
 
-        return merged_word
+        return merged_string
--- run.py
+++ run.py
@@ -2,10 +2,11 @@ import sys
 import importlib
 import inspect
 import json
+import copy
 
 
 def run():
-    # Get problem ID and input file from terminal.
+    # Get problem ID and input file path from command line.
     problem_id = sys.argv[1]
     data_file = sys.argv[2]
 
@@ -17,7 +18,7 @@ def run():
     Solution = module.Solution
     object = Solution()
 
-    # Get the names of methods in the Solution class.
+    # Get the names of methods in the Solution class in order of appearance.
     methods = []
     for name, func in Solution.__dict__.items():
         if not name.startswith("__") and inspect.isfunction(func):
@@ -28,18 +29,29 @@ def run():
     # Load and parse problem data.
     with open(data_file, "r") as f:
         data = json.load(f)
-        output = data['output']
-        tests = data['tests']
+        output = data["output"]
+        tests = data["tests"]
 
     # Run and print the result.
+    print()
+    cnt = 1
     for input in tests:
+        old_input = copy.deepcopy(input)
+        old_input = ", ".join(map(str, old_input))
+
         result = method(*input)
-        if output == 'return':
-            print(result)
+
+        print(f"  test: {cnt}")
+        print(f" input: {old_input}")
+        if output == "return":
+            print(f"output: {result}")
         else:
             args = list(inspect.signature(method).parameters.keys())
             arg_index = args.index(output)
-            print(*input[arg_index])
+            print(f"output: {input[arg_index]}")
+        print()
+        
+        cnt += 1
 
 
 if __name__ == "__main__":
